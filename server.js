91921f33630600cc09756b4d4ce002e78ed683a1
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-04-15 23:48:54 +0530

    refactor: clean up whitespace and improve comment clarity in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
-   
+      
 connectDB();
-
+//i connected to the website
 app.listen(3000,() => console.log("server up at 3000"));
